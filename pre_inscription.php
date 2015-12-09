506ba735feb6a37742e198059dd0979b8f66f712
clemge <clemence.geli@gmail.com>
2015-12-09 17:02:40 +0100

    pre-inscription

---
+++ pre_inscription.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Protoss-type Soccer Five</title>
+
+		<!-- Bootstrap -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+		</head>
+	<body style="background-color:#D9D9D9">
+  	
+  			<div style="height:20px;"></div>
+				<div class="row" style="text-align:center;" style="color:black">
+					<div class="checkbox">
+						<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4"></div>
+						<div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+							<div class="alert alert-info" style="background-color:white" style="color:black">
+							<strong>PRE-INSCRIPTION</strong></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br />
+			<br />
+			<br />
+			<div class="row">
+				<div class="form-group">
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="background-color:#ABABAB; text-align:center;"><label for="nom"> Nom </label></div>
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><label for="nom"><textarea  rows="1"></textarea></div></div>
+				<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5"></div>
+				</div>
+			</div>
+
+			<div style="height:20px;"></div>
+			<div style="height:20px;"></div>
+
+			<div class="row">
+				<div class="form-group">
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="background-color:#ABABAB; text-align:center;"><label for="prénom"> Prénom </label></div>
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><label for="nom"><textarea  rows="1"></textarea></div></div>
+				<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5"></div>
+				</div>
+			</div>
+
+			<div style="height:20px;"></div>
+			<div style="height:20px;"></div>
+
+			<div class="row">
+				<div class="form-group">
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="background-color:#ABABAB; text-align:center;"><label for="email"> Adresse mail </label></div>
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><input type="email" id="email" placeholder="Adresse mail"></div>
+				<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5"></div>
+				</div>
+			</div>
+
+			<div style="height:20px;"></div>
+			<div style="height:20px;"></div>
+
+			<div class="row">
+				<div class="form-group">
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="background-color:#ABABAB; text-align:center;"><label for="mdp"> Mot de passe </label></div>
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><input type="password" id="mdp" placeholder="Mot de passe"></div>
+				<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5"></div>
+				</div>
+			</div>
+
+			<div style="height:20px;"></div>
+			<div style="height:20px;"></div>
+
+			<div class="row">
+				<div class="form-group">
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="background-color:#ABABAB; text-align:center;"><label for="ville"> Ville </label></div>
+				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
+				<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"><label for="ville"><textarea rows="1"></textarea></div></div>
+				<div class="col-xs-5 col-sm-5 col-md-5 col-lg-5"></div>
+				</div>
+			</div>
+
+
+			<div style="height:20px;"></div>
+			<div style="height:20px;"></div>
+
+
+			<div style="height:20px;"></div>
+			<div class="row" style="text-align:center;">
+				<div class="checkbox">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="alert alert-info" style="background-color:#00C9D4;">
+						<button type="submit" style="background-color:#00C9D4;"><strong>Envoyer</strong> </button></div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+			<div class="checkbox">
+				<div class="form-group">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"style="text-align:center;"><label><input type="checkbox"> Rester connecté </label>
+			</div>
+			</div>
+
